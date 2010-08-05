f62dc5e7cb560fd009df20681356589791ef673a
S. M. ARIFUL ISLAM <arif@mygithub-office.com>
2010-08-05 23:03:08 +0600

    added New template files in repo

---
+++ new_template/html/js/dropdownmenu.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+<!--//--><![CDATA[//><!--
+sfHover = function() {
+	var sfEls = document.getElementByClass("containerNavigation").getElementsByTagName("LI");
+	for (var i=0; i<sfEls.length; i++) {
+		sfEls[i].onmouseover=function() {
+			this.className+=" sfhover";
+		}
+		sfEls[i].onmouseout=function() {
+			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+		}
+	}
+}
+if (window.attachEvent) window.attachEvent("onload", sfHover);
+
+//--><!]]>
